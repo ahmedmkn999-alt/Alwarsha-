aa8f2c22f70e4deafbfa8ea67f84d3a4cff99690
ahmedmkn999-alt <ahmedmkn999@gmail.com>
2026-01-20 03:29:46 +0200

    Add Next.js configuration with image domains

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['firebasestorage.googleapis.com', 'placehold.co'],
+  },
+}
+
+module.exports = nextConfig
